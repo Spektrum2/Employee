cf338d5bda269bd6e976686e027f41a7f328fd03
admin <promitay@gmail.com>
2022-07-21 14:16:12 +0300

    Домашняя работа 2.5. Коллекции и сеты

---
+++ src/main/java/pro/sky/java/course2/employee/controller/EmployeeController.java
@@ -0,0 +1,74 @@
+package pro.sky.java.course2.employee.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.java.course2.employee.domain.Employee;
+import pro.sky.java.course2.employee.service.EmployeeService;
+import pro.sky.java.course2.exceptions.EmployeeAlreadyAddedException;
+import pro.sky.java.course2.exceptions.EmployeeNotFoundException;
+import pro.sky.java.course2.exceptions.EmployeeStorageIsFullException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> printEmployee() {
+        return employeeService.printEmployee();
+    }
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam("firstName") String name,
+                              @RequestParam("lastName") String surname) {
+        Employee employee = new Employee(
+                name,
+                surname
+        );
+        if (employeeService.printListSize() >= 10) {
+            throw new EmployeeStorageIsFullException();
+        }else if(employeeService.findEmployee(employee) != null) {
+            throw new EmployeeAlreadyAddedException();
+        } else {
+            employeeService.addEmployee(employee);
+            return employee;
+        }
+    }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam("firstName") String name,
+                                 @RequestParam("lastName") String surname) {
+        Employee employee = new Employee(
+                name,
+                surname
+        );
+        if (employeeService.removeEmployee(employee)) {
+            return employee;
+        } else {
+            throw new EmployeeNotFoundException();
+        }
+
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam("firstName") String name,
+                                 @RequestParam("lastName") String surname) {
+        Employee employee = new Employee(
+                name,
+                surname
+        );
+        if (employeeService.findEmployee(employee) == null) {
+            throw new EmployeeNotFoundException();
+        } else {
+            return employeeService.findEmployee(employee);
+        }
+    }
+}
---
+++ src/main/java/pro/sky/java/course2/employee/service/EmployeeService.java
@@ -0,0 +1,54 @@
+package pro.sky.java.course2.employee.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.java.course2.employee.domain.Employee;
+import pro.sky.java.course2.exceptions.EmployeeAlreadyAddedException;
+import pro.sky.java.course2.exceptions.EmployeeNotFoundException;
+import pro.sky.java.course2.exceptions.EmployeeStorageIsFullException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    List<Employee> employees = new ArrayList<>(List.of(
+            new Employee("Марк", "Захаров"),
+            new Employee("София", "Нефедова"),
+            new Employee("Алексей", "Дементьев"),
+            new Employee("Александр", "Левин"),
+            new Employee("Майя", "Михайлова"),
+            new Employee("Майя", "Попова"),
+            new Employee("Злата", "Мещерякова"),
+            new Employee("Дмитрий", "Федоров"),
+            new Employee("Георгий", "Гаврилов"),
+            new Employee("Мадина", "Яковлева")
+    ));
+
+    public List<Employee> printEmployee() {
+        return employees;
+    }
+
+
+    public Integer printListSize() {
+        return employees.size();
+    }
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+
+    public boolean removeEmployee(Employee employee) {
+        return employees.remove(employee);
+    }
+
+
+    public Employee findEmployee(Employee employee) {
+
+        for (Employee worker : employees) {
+            if (worker.equals(employee)) {
+                return worker;
+            }
+        }
+        return null;
+    }
+}
